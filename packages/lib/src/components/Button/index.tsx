d61be89ff144a758764380745e5b852073300c17
dbarabashdev <dbarabashdev@gmail.com>
2020-01-23 19:03:42 +0200

    lerna init

---
+++ packages/lib/src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledButton = styled.button`
+  background: red;
+`
+
+export const Button = () => <StyledButton>Hello this is button</StyledButton>
